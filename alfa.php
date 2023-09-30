0517b6aee3a585019137f858e0c901d354cb7666
Som98400 <som.themegrill@gmail.com>
2023-09-30 08:55:55 +0545

    add plugin main file

---
+++ alfa.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Alpha
+ * Plugin URI: https://wprdpress.org/plugins
+ * Description: Alpha Plugin For CRUD operation WordPress.
+ * Version: 1.0.0
+ * Author: Som Shrestha
+ * Author URI:
+ * Text Domain: alpha
+ * Domain Path: /languages/
+ *
+ * Copyright: © 2023 Som.
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package Alpha
+ */
